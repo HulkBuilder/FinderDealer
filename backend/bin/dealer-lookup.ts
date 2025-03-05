46ffe9212b8551fe7903137ac37fc24029901c5f
HulkBuilder <61037904+HulkBuilder@users.noreply.github.com>
2025-03-04 23:03:04 -0600

    Add files via upload

--- backend/bin/dealer-lookup.ts
+++ backend/bin/dealer-lookup.ts
@@ -1,1 +1,6 @@
-// Entry point for CDK
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DealerLookupStack } from '../lib/dealer-lookup-stack';
+
+const app = new cdk.App();
+new DealerLookupStack(app, 'DealerLookupStack', {});
